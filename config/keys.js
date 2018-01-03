3e99c5686520c94c208e70cfd373e19225061043
Chaitanya <karanam.krishna.25@gmail.com>
2018-01-03 22:03:44 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports={
-    serviceNowClientID: '1f7ea073a5130300ff87eea996cdefde',
-    serviceNowClientSecret: '9N(0&0-Vc^',
-    key:'sdhaldhalsdhlkdhlKDSLKHDLKHADJ'
+    serviceNowClientID: '<CLIENT ID FROM SERVICENOW>',
+    serviceNowClientSecret: '<CLIENT SECRET FROM SERVICENOW>',
+    key:'<ANYRANDOMSTRING>'
 }
